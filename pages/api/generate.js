57e74ed0df317bd5d75a7187ee5821865596a3c1
F041 <contact.ggiordano@gmail.com>
2023-03-23 16:46:42 +0100

    updated

---
+++ pages/api/generate.js
@@ -0,0 +1,55 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Scrivimi qualcosa sulla città di Mileto, situata in provincia di Vibo Valentia
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Scrivi delle storie riguardo Mileto.
+
+  Titolo: ${req.body.userInput}
+
+  Tabella di contenuti: ${basePromptOutput.text}
+
+  Storia:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 550,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -4,35 +4,76 @@ import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+  
+    const data = await response.json();
+    const { output } = data;
+    
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
   const onUserChangedText = (event) => {
-    console.log(event.target.value);
     setUserInput(event.target.value);
   };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 project</title>
+        <title>MiletoGPT</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>this will get a text later</h1>
+            <h1>la tua guida per la capitale Normanna</h1>
           </div>
           <div className="header-subtitle">
             <h2>as a later subtitle as well</h2>
           </div>
         </div>
-        {/* Add this code here*/}
         <div className="prompt-container">
         <textarea
-            className="prompt-box"
-            placeholder="start typing here"
-            value={userInput}
-            onChange={onUserChangedText}
-          />
+        placeholder="chiedi qualcosa su Mileto (VV)"
+        className="prompt-box"
+        value={userInput}
+        onChange={onUserChangedText}
+        />
+        <div className="prompt-buttons">
+          <a
+          className={isGenerating ? 'generate-button loading' : 'generate-button'}
+          onClick={callGenerateEndpoint}
+        >
+          <div className="generate">
+          {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+          </div>
+        </a>
         </div>
-      </div>
-    </div>
+          {/* New code I added here */}
+          {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+                </div>
+              )}
+              </div>
+                </div>
+        </div>
   );
 };
 
